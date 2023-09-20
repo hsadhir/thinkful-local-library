51c50285ed6901bf89ce974a5bbf2e11e61b79a4
Hasan Sadhir Mohammed Mansoor <hsadhir@indeed.com>
2023-09-20 22:06:40 +0530

    Complete qualified assignment

---
+++ public/src/accounts.js
@@ -0,0 +1,51 @@
+// concise arrow function
+const findAccountById = (accounts, id) => accounts.find(account => account.id === id);
+
+function sortAccountsByLastName(accounts) {
+  // YOUR SOLUTION HERE
+  // Hint: You can use the [`sort()`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort) method here. 
+  accounts.sort((prev, curr) => prev.name.last > curr.name.last ? 1 : -1);
+  return accounts;
+}
+
+// named function expression
+const getAccountFullNames = function getFullNames(accounts) {
+  // YOUR SOLUTION HERE
+  // Hint: You can use the [`map()`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map) method here.
+  return accounts.map(account => {
+    // nested object destructuring with custom name for variables.
+    const { name: { first: firstName, last: lastName } } = account;
+    return `${firstName} ${lastName}`;
+  });
+}
+
+// NOTE: YOU DON'T HAVE TO EDIT THE FUNCTIONS BELOW
+function getTotalNumberOfBorrows(account, books) {
+  return books.reduce((acc, book) => {
+    const count = book.borrows.reduce((borrowAcc, borrow) => {
+      return borrow.id === account.id ? borrowAcc + 1 : borrowAcc;
+    }, 0);
+
+    return acc + count;
+  }, 0);
+}
+
+function getBooksPossessedByAccount(account, books, authors) {
+  return books
+    .filter((book) => {
+      const recent = book.borrows[0];
+      return !recent.returned && recent.id === account.id;
+    })
+    .map((book) => {
+      const author = authors.find((author) => author.id === book.authorId);
+      return { ...book, author };
+    });
+}
+
+module.exports = {
+  findAccountById,
+  sortAccountsByLastName,
+  getAccountFullNames,
+  getTotalNumberOfBorrows,
+  getBooksPossessedByAccount,
+};
---
+++ public/src/books.js
@@ -0,0 +1,50 @@
+// anonymous function expression
+const findAuthorById = function (authors, id) {
+  // Have used a for/of loop to find author's id deliberately.
+  // this could be done using a find, which I have used in a similar function below.
+  for (const author of authors) {
+    if (author.id === id) return author;
+  }
+  return;
+}
+
+function findBookById(books, id) { return books.find(book => book.id === id); }
+
+// NOTE: YOU DON'T HAVE TO EDIT THE FUNCTIONS BELOW
+function partitionBooksByBorrowedStatus(books) {
+  return books.reduce(
+    (acc, book) => {
+      const [borrowed, returned] = acc;
+      const recent = book.borrows[0];
+      if (recent.returned) {
+        returned.push(book);
+      } else {
+        borrowed.push(book);
+      }
+
+      return acc;
+    },
+    [[], []]
+  );
+}
+
+function getBorrowersForBook(book, accounts) {
+  const accountsById = accounts.reduce((acc, account) => {
+    acc[account.id] = account;
+    return acc;
+  }, {});
+
+  return book.borrows
+    .map(({ id, returned }) => ({
+      ...accountsById[id],
+      returned,
+    }))
+    .slice(0, 10);
+}
+
+module.exports = {
+  findAuthorById,
+  findBookById,
+  partitionBooksByBorrowedStatus,
+  getBorrowersForBook,
+};
